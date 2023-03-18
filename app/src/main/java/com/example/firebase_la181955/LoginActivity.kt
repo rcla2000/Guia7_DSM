0bbabfea6260c7708d3b17388f46b3c324f445f3
Roberto Carlos López Abarca <rcla200031@gmail.com>
2023-03-17 21:00:59 -0600

    Autenticacion con Google agregada

--- app/src/main/java/com/example/firebase_la181955/LoginActivity.kt
+++ app/src/main/java/com/example/firebase_la181955/LoginActivity.kt
@@ -8,20 +8,34 @@ import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.GoogleAuthProvider
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var mGoogleSignInClient : GoogleSignInClient
+    val Req_Code : Int = 123
     private lateinit var auth : FirebaseAuth
     private lateinit var btnLogin : Button
+    private lateinit var btnFacebook : Button
+    private lateinit var btnGoogle : Button
     private lateinit var textViewRegister : TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        FirebaseApp.initializeApp(this)
 
         auth = FirebaseAuth.getInstance()
-
         btnLogin = findViewById<Button>(R.id.btnLogin)
+        btnFacebook = findViewById<Button>(R.id.btnFacebook)
+        btnGoogle = findViewById<Button>(R.id.btnGoogle)
+
         btnLogin.setOnClickListener {
             val email = findViewById<EditText>(R.id.editTextEmailAddress).text.toString()
             val password = findViewById<EditText>(R.id.txtPassword).text.toString()
@@ -31,8 +45,53 @@ class LoginActivity : AppCompatActivity() {
         textViewRegister.setOnClickListener {
             this.goToRegister()
         }
+        val googleSign = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, googleSign)
+        btnGoogle.setOnClickListener {
+            signInGoogle()
+        }
     }
 
+    private fun signInGoogle() {
+        val signInIntent : Intent = mGoogleSignInClient.signInIntent
+        startActivityForResult(signInIntent, Req_Code)
+    }
+
+    // Datos de la cuenta de Google
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == Req_Code) {
+            val task : Task<GoogleSignInAccount> = GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleResult(task)
+        }
+    }
+
+    private fun handleResult(completedTask : Task<GoogleSignInAccount>) {
+        try {
+            val account : GoogleSignInAccount? = completedTask.getResult(ApiException::class.java)
+            if (account != null) {
+                loginGoogle(account)
+            }
+        } catch (e: ApiException) {
+            Toast.makeText(this, "Ha ocurrido un error", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun loginGoogle(account : GoogleSignInAccount) {
+        val credencial = GoogleAuthProvider.getCredential(account.idToken, null)
+        auth.signInWithCredential(credencial).addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                val intent = Intent(this, MainActivity::class.java)
+                intent.putExtra("tipoLogin", "google")
+                startActivity(intent)
+                finish()
+            }
+        }
+    }
     private fun login(email : String, password: String) {
         auth.signInWithEmailAndPassword(email, password).addOnCompleteListener { task ->
             if (task.isSuccessful) {
--- app/src/main/java/com/example/firebase_la181955/MainActivity.kt
+++ app/src/main/java/com/example/firebase_la181955/MainActivity.kt
@@ -6,12 +6,19 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mGoogleSignInClient: GoogleSignInClient
+    var tipoLogin : String =  ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val bundle = intent.extras
+        tipoLogin = bundle?.getString("tipoLogin").toString()
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -22,11 +29,28 @@ class MainActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             R.id.action_sign_out -> {
-                FirebaseAuth.getInstance().signOut().also {
-                    Toast.makeText(this, "Sesión cerrada", Toast.LENGTH_SHORT).show()
-                    val intent = Intent(this, RegisterActivity::class.java)
-                    startActivity(intent)
-                    finish()
+                if (tipoLogin == "google") {
+                    val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                        .requestIdToken(getString(R.string.default_web_client_id))
+                        .requestEmail()
+                        .build()
+                    mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+                    mGoogleSignInClient.signOut().addOnCompleteListener {
+                        Toast.makeText(this, "Sesón de Google cerrada", Toast.LENGTH_SHORT).show()
+                        FirebaseAuth.getInstance().signOut().also {
+                            Toast.makeText(this, "Sesión cerrada", Toast.LENGTH_SHORT).show()
+                            val intent = Intent(this, RegisterActivity::class.java)
+                            startActivity(intent)
+                            finish()
+                        }
+                    }
+                } else {
+                    FirebaseAuth.getInstance().signOut().also {
+                        Toast.makeText(this, "Sesión cerrada", Toast.LENGTH_SHORT).show()
+                        val intent = Intent(this, RegisterActivity::class.java)
+                        startActivity(intent)
+                        finish()
+                    }
                 }
             }
         }
